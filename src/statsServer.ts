71dac1e0575710bf6095bc3782ac237908f67904
pagnkelly <632228198@qq.com>
2022-07-30 23:48:03 +0800

    feat: body

--- src/statsServer.ts
+++ src/statsServer.ts
@@ -3,7 +3,14 @@ module.exports = (server: any) => {
     const oReq = req.originalReq;
     if (oReq.url.startsWith('https://omgup.hongyibo.com.cn/api/smallstat/x')) {
       console.log(oReq.url, 'req')
-      console.log(oReq)
+      req.getSession((s:any) => {
+        // 如果设置了 enable://hide 会获取到空数据
+        console.log(s.req.body, 'ssss')
+        if (!s) {
+          return;
+        }
+        // do sth
+      })
     }
   })
 }
